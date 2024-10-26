0a375ca4cdb67fb143cdde5af559a9a7b1da5e52
Rukh94 <abdulganiyufarouk89@gmail.com>
2024-10-26 16:22:50 +0100

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+
+async function setupNodeEvents(on, config) {
+await preprocessor.addCucumberPreprocessorPlugin(on, config);
+on("file:preprocessor", browserify.default(config));
+return config;
+}
+module.exports = defineConfig({
+e2e: {
+setupNodeEvents,
+specPattern: 'cypress/integration/*.feature',
+},
+});
